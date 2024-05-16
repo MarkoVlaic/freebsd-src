55c7b7a2dec448789e0e6ce8f0651e477a13096d
MarkoVlaic <vlaic.marko@gmail.com>
2024-05-16 14:48:12 +0200

    modify sysctl

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -2,6 +2,6 @@
 #include <sys/sysctl.h>
 #include <sys/zcond.h>
 
-SYSCTL_INT(_kern, OID_AUTO, one, CTLFLAG_RD, NULL, 1, "Always returns one");
+SYSCTL_INT(_kern, OID_AUTO, one, CTLFLAG_RD, SYSCTL_NULL_INT_PTR, 1, "Always returns one");
 
 

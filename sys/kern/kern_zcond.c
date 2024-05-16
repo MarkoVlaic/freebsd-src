855091f32f76ee47f6dba3a5a142e3b1cef6aa41
MarkoVlaic <vlaic.marko@gmail.com>
2024-05-16 15:13:08 +0200

    trigger zcond test with sysctl

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -6,6 +6,27 @@
 #include <sys/sysctl.h>
 #include <sys/zcond.h>
 
-SYSCTL_INT(_kern, OID_AUTO, one, CTLFLAG_RD, SYSCTL_NULL_INT_PTR, 1, "Always returns one");
+DEFINE_ZCOND_TRUE(cond1);
+DEFINE_ZCOND_FALSE(cond2);
 
+static int 
+trigger_zcond_test(SYSCTL_HANDLER_ARGS) {
+    if(zcond_true(cond1)) {
+        printf("cond 1 true\n");
+    }
 
+    if(zcond_false(cond1)) {
+        printf("cond 1 false\n");
+    }
+    
+    if(zcond_true(cond2)) {
+        printf("cond 2 true\n");
+    }
+
+    if(zcond_false(cond2)) {
+        printf("cond 2 false\n");
+    }
+    return 0;
+}
+
+SYSCTL_PROC(_kern, OID_AUTO, zcond, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, SYSCTL_NULL_INT_PTR, trigger_zcond_test, "I", "trigger zcond test");

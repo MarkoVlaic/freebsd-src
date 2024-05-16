31f8106eaf73327809aa889a7e7561c3a59cc900
MarkoVlaic <vlaic.marko@gmail.com>
2024-05-16 14:39:07 +0200

    fix typo

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -2,6 +2,6 @@
 #include <sys/sysctl.h>
 #include <sys/zcond.h>
 
-SYSCTL_INT(_kern, OID_AUTO, one, CTFLAGS_RD, NULL, 1, "Always returns one");
+SYSCTL_INT(_kern, OID_AUTO, one, CTLFLAGS_RD, NULL, 1, "Always returns one");
 
 

208218f423d1862233873eba8d505dee0e45fff4
MarkoVlaic <vlaic.marko@gmail.com>
2024-05-23 11:42:37 +0200

    try relative addressing

--- sys/amd64/include/zcond.h
+++ sys/amd64/include/zcond.h
@@ -5,8 +5,8 @@
 
 #define ZCOND_TABLE_ENTRY \
     ".pushsection __zcond_table, \"aw\" \n\t"  \
-    ".long 1b \n\t" \
-    ".long %l[l_true] \n\t" \
+    ".long 1b - . \n\t" \
+    ".long %l[l_true] - . \n\t" \
     ".popsection \n\t"
 
 struct zcond;

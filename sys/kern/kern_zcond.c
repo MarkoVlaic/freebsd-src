4f3567bee2eb6e1346a4dedc264e93499b2dc635
MarkoVlaic <vlaic.marko@gmail.com>
2024-06-28 18:39:16 +0200

    sysctl

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -118,7 +118,13 @@ static int zcond_list_inspection_points(SYSCTL_HANDLER_ARGS) {
     return 0;
 }
 
+static int zcond1_disable(SYSCTL_HANDLER_ARGS) {
+    zcond_disable(cond1);
+    printf("disabled\n");
+}
+
 SYSCTL_PROC(_kern, OID_AUTO, zcond, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, 0, trigger_zcond_test, "I", "trigger zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond2, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, 0, trigger_zcond_test2, "I", "trigger second zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond3, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, 0, trigger_zcond_test3, "I", "trigger third zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond_ins_p, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, 0, zcond_list_inspection_points, "I", "list cond1 inspection points");
+SYSCTL_PROC(_kern, OID_AUTO, zcond1_disable, CTLFLAG_RW | CTLTYPE_INT, SYSCTL_NULL_INT_PTR, 0, zcond1_disable, "I", "disable zcond1");

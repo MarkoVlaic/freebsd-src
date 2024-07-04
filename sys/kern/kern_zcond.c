8bc6ddcaa67777254ec7dc5f05122f9cf43deedf
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-04 15:27:59 +0200

    missing include

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -9,6 +9,7 @@
 #include <vm/vm.h>
 #include <vm/vm_page.h>
 #include <vm/pmap.h>
+#include <sys/x86/include/x86_var.h>
 
 MALLOC_DECLARE(M_ZCOND);
 MALLOC_DEFINE(M_ZCOND, "zcond", "malloc for the zcond subsystem");

79ce5127ed9c743056b15410be01b5e09ee6f963
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-05 14:12:50 +0200

    fix build errors

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -51,40 +51,42 @@ void __zcond_disable(struct zcond* cond) {
     }
     
     struct ins_point *p;
+    char* patch_addr;
     char insn[5];
     size_t insn_size;
     SLIST_FOREACH(p, &cond->ins_points, next) {
         bool wp = disable_wp();
+        patch_addr = (char*) p->patch_addr;
 
         if(p->ins_type == INS_TYPE_FALSE) {
             // replace nop with jmp
             size_t offset;
-            if(*p->patch_addr == 0x66) {
+            if(*patch_addr == 0x66) {
                 // two byte nop
-               size = 2;
-            } else if(*p->patch_addr == 0x0f) {
-                size = 5
+               insn_size = 2;
+            } else if(patch_addr == 0x0f) {
+                insn_size = 5
             } else {
                 panic("unexpected opcode: %02hhx", *p->patch_addr); 
             }
 
-            offset = p->lbl_true_addr - p->patch_addr + size; 
-            arch_insn_jmp(insn, size, offset);
+            offset = p->lbl_true_addr - p->patch_addr + insn_size; 
+            arch_insn_jmp(insn, insn_size, offset);
         } else {
             //  replace jmp with nop
-            if(*p->patch_addr == 0xeb) {
+            if(*patch_addr == 0xeb) {
                 // two byte jump
-                size = 2;
-            } else if(*p->patch_addr == 0xe9) {
+                insn_size = 2;
+            } else if(*patch_addr == 0xe9) {
                 // five byte jump
-                size = 5;
+                insn_size = 5;
             } else {
                 panic("unexpected opcode: %02hhx", *p->patch_addr); 
             }
-            arch_insn_nop(insn, size);
+            arch_insn_nop(insn, insn_size);
         }
 
-        memcpy((void *)p->patch_addr, &insn[0], size);
+        memcpy((void *)patch_addr, &insn[0], insn_size);
         restore_wp(wp);
     }
     cond->enabled = false;

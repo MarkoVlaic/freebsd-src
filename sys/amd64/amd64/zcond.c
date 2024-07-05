600f26ed09f6d89056fef78b3e54ce0b778743c9
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-05 14:18:19 +0200

    fix even more build errors

---
+++ sys/amd64/amd64/zcond.c
@@ -0,0 +1,27 @@
+#include <machine/zcond.h>
+
+void arch_insn_nop(unsigned char insn[], size_t size) {
+    int i;
+    if(size == 2) {
+        for(i=0;i<2;i++) {
+            insn[i] = nop2_bytes[i];
+        }
+    } else {
+        for(i=0;i<5;i++) {
+            insn[i] = nop5_bytes[i];
+        }
+    }
+}
+
+void arch_insn_jmp(unsigned char insn[], size_t size, size_t offset) {
+    if(size == 2) {
+        insn[0] = 0xeb;
+        insn[1] = offset;
+    } else {
+        insn[0] = 0xe9;
+        int i;
+        for(i=0;i<4;i++) {
+            insn[i+1] = (offset >> (i*2)) & 0xFF;
+        }
+    }
+}

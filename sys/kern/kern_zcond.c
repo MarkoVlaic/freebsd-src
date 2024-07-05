97ae54d132c51462b9ba4e869c402667d0891de7
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-05 16:34:25 +0200

    more printf

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -69,9 +69,10 @@ void __zcond_disable(struct zcond* cond) {
             } else {
                 panic("unexpected opcode: %02hhx", *patch_addr); 
             }
-
+            
             offset = p->lbl_true_addr - p->patch_addr + insn_size; 
             arch_insn_jmp(insn, insn_size, offset);
+            printf("offset = %#08lx\n", offset);
         } else {
             //  replace jmp with nop
             if(*patch_addr == 0xeb) {

0ad57f3c885cec7110da123c677dd39f04975e2e
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-06 13:21:43 +0200

    change the tests and add a second zcond

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -154,9 +154,7 @@ void __zcond_disable(struct zcond* cond) {
 }
 
 DEFINE_ZCOND_TRUE(cond1);
-// ZCOND_INIT(&cond1); where should i call this?
 DEFINE_ZCOND_FALSE(cond2);
-// ZCOND_INIT(&cond2);
 
 
 static int 
@@ -167,25 +165,19 @@ trigger_zcond_test(SYSCTL_HANDLER_ARGS) {
     sbuf_printf(&buf, "zcond test start\n");
     if(zcond_true(cond1)) {
         sbuf_printf(&buf, "cond 1 true\n");
-        asm (
-            ".nops 512\n\t":::    
-        );
+    } else {
+        sbuf_printf(&buf, "cond 1 false\n");
     }
-    
-    sbuf_finish(&buf);
-    sbuf_delete(&buf);
-    
-    /*if(zcond_false(cond1)) {
-        printf("cond 1 false\n");
-    }*/
-    
-   /* if(zcond_true(cond2)) {
-        printf("cond 2 true\n");
+   
+    if(zcond_true(cond2)) {
+        sbuf_printf(&buf, "cond2 true\n");
+    } else {
+        sbuf_printf(&buf, "cond2 false\n");
     }
 
-    if(zcond_false(cond2)) {
-        printf("cond 2 false\n");
-    }*/
+    sbuf_finish(&buf);
+    sbuf_delete(&buf);
+
     return 0;
 }
 
@@ -197,6 +189,8 @@ static int trigger_zcond_test2(SYSCTL_HANDLER_ARGS) {
     if(zcond_true(cond1)) {
        sbuf_printf(&buf, "cond 1 true %s\n", __func__);
     }
+    
+    // simulate long jump with nops
     asm (
         ".nops 512\n\t":::    
     );
@@ -212,9 +206,15 @@ static int trigger_zcond_test3(SYSCTL_HANDLER_ARGS) {
     
     sbuf_printf(&buf, "zcond test 3 start\n");
     if(zcond_false(cond1)) {
-        sbuf_printf(&buf, "cond 1 false\n");
+        sbuf_printf(&buf, "cond1 false\n");
     } else {
-        sbuf_printf(&buf, "else branch\n");
+        sbuf_printf(&buf, "cond1 true\n");
+    }
+
+    if(zcond_false(cond2)) {
+        sbuf_printf(&buf, "cond2 false\n");
+    } else {
+        sbuf_printf(&buf, "cond2 true\n");
     }
 
     sbuf_finish(&buf);
@@ -244,7 +244,7 @@ static int zcond1_disable(SYSCTL_HANDLER_ARGS) {
     sbuf_new_for_sysctl(&buf, NULL, 256, req);
 
     zcond_disable(cond1);
-    sbuf_printf(&buf, "disabled\n");
+    sbuf_printf(&buf, "cond1 disabled\n");
 
     return 0;
 }
@@ -255,7 +255,7 @@ static int zcond1_enable(SYSCTL_HANDLER_ARGS) {
     sbuf_new_for_sysctl(&buf, NULL, 256, req);
     
     zcond_enable(cond1);
-    sbuf_printf(&buf, "enabled\n");
+    sbuf_printf(&buf, "cond1 enabled\n");
     
     sbuf_finish(&buf);
     sbuf_delete(&buf);
@@ -263,9 +263,34 @@ static int zcond1_enable(SYSCTL_HANDLER_ARGS) {
     return 0;
 }
 
+static int zcond2_disable(SYSCTL_HANDLER_ARGS) {
+    struct sbuf buf;
+    sbuf_new_for_sysctl(&buf, NULL, 256, req);
+
+    zcond_disable(cond2);
+    sbuf_printf(&buf, "cond2 disabled\n");
+
+    return 0;
+}
+
+
+static int zcond2_enable(SYSCTL_HANDLER_ARGS) { 
+    struct sbuf buf;
+    sbuf_new_for_sysctl(&buf, NULL, 256, req);
+    
+    zcond_enable(cond2);
+    sbuf_printf(&buf, "cond2 enabled\n");
+    
+    sbuf_finish(&buf);
+    sbuf_delete(&buf);
+
+    return 0;
+}
 SYSCTL_PROC(_kern, OID_AUTO, zcond, CTLFLAG_RD | CTLTYPE_STRING, NULL, 0, trigger_zcond_test, "I", "trigger zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond2, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, trigger_zcond_test2, "I", "trigger second zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond3, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, trigger_zcond_test3, "I", "trigger third zcond test");
 SYSCTL_PROC(_kern, OID_AUTO, zcond_ins_p, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, zcond_list_inspection_points, "I", "list cond1 inspection points");
 SYSCTL_PROC(_kern, OID_AUTO, zcond1_enable, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, zcond1_enable, "I", "enable zcond1");
 SYSCTL_PROC(_kern, OID_AUTO, zcond1_disable, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, zcond1_disable, "I", "disable zcond1");
+SYSCTL_PROC(_kern, OID_AUTO, zcond2_enable, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, zcond2_enable, "I", "enable zcond2");
+SYSCTL_PROC(_kern, OID_AUTO, zcond2_disable, CTLFLAG_RD | CTLTYPE_STRING, SYSCTL_NULL_INT_PTR, 0, zcond2_disable, "I", "disable zcond2");

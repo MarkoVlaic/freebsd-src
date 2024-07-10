2357fc9780b5499e1f75ee4892dc594174fee31c
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-10 12:23:33 +0200

    reorder includes

--- sys/amd64/amd64/zcond.c
+++ sys/amd64/amd64/zcond.c
@@ -1,10 +1,9 @@
 #include <machine/zcond.h>
 #include <sys/zcond.h>
-#include <machine/svm.h>
-#include <machine/md_var.h>
-#include <machine/machdep.h>
 #include <vm/vm.h>
 #include <vm/pmap.h>
+#include <machine/md_var.h>
+#include <machine/machdep.h>
 
 static bool wp;
 void arch_enable_text_write(void) {

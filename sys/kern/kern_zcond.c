60d72b709f0340e2a1f8f6c6189608801bf5c222
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-23 15:38:14 +0200

    uncomment pmap_enter

--- sys/kern/kern_zcond.c
+++ sys/kern/kern_zcond.c
@@ -71,7 +71,7 @@ zcond_init(const void* unused) {
     patch_page_mirror = vm_page_alloc_noobj(VM_ALLOC_WIRED);
     mirror_page_addr = 0xffffffff81a01000;
     //mirror_page_addr = kva_alloc(PAGE_SIZE);
-    //pmap_enter(&patching_pmap, mirror_page_addr, patch_page_mirror, VM_PROT_WRITE, PMAP_ENTER_WIRED, 0);
+    pmap_enter(&patching_pmap, mirror_page_addr, patch_page_mirror, VM_PROT_WRITE, PMAP_ENTER_WIRED, 0);
 }
 SYSINIT(zcond, SI_SUB_LAST, SI_ORDER_ANY, zcond_init, NULL); // do we declare a new SI_SUB? is the order important?
 

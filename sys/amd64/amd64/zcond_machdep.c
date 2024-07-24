f4773d6d0b8bf666d2d5ec81e86ac9f46b7becf4
MarkoVlaic <vlaic.marko@gmail.com>
2024-07-24 13:57:28 +0200

    build errors

--- sys/amd64/amd64/zcond_machdep.c
+++ sys/amd64/amd64/zcond_machdep.c
@@ -1,13 +1,10 @@
-#include <sys/zcond.h>
 
 #include <vm/vm.h>
 #include <vm/pmap.h>
 
 #include <machine/cpufunc.h>
-#include <machine/md_var.h>
 #include <machine/zcond.h>
 
-#include <amd64/vmm/amd/svm.h>
 
 // static bool wp;
 static uint64_t cr3;

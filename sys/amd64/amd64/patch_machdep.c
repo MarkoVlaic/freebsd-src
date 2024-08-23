a9c78c036d8a62fcb954a59c715d314123d33c88
MarkoVlaic <vlaic.marko@gmail.com>
2024-08-23 08:39:56 +0200

    printf debugging

--- sys/amd64/amd64/patch_machdep.c
+++ sys/amd64/amd64/patch_machdep.c
@@ -96,6 +96,7 @@ patch_init_pte(void)
 static void
 patch_pmap_init(const void *unused)
 {
+	printf("kernel addr | ptr = %p | offset = %lx | size = %zu\n", linker_kernel_file->address, (vm_offset_t) linker_kernel_file->address, linker_kernel_file->size);
 	memset(&patch_pmap, 0, sizeof(patch_pmap));
 	PMAP_LOCK_INIT(&patch_pmap);
 	pmap_pinit(&patch_pmap);
